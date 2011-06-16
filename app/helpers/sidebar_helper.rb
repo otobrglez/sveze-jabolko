38a359f571e7a9087479a933e7d9c5c5ba721714
Oto Brglez <otobrglez@gmail.com>
2011-06-16 14:32:49 +0200

    Latests forum posts added

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -3,5 +3,12 @@ module SidebarHelper
   def sidebar_top_articles(limit=10)
     nil
   end
+  
+  def sidebar_forum_posts(limit=7)
+    require "net/http"
+    url = "http://forum.jabolko.org/izmenjava.php?a=posts"
+    posts = JSON.parse(Net::HTTP.get_response(URI.parse(url)).body).slice(0,limit)
+    posts
+  end
 
 end
