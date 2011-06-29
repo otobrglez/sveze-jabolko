e1f1977a92d05f3a06db973743dc77afa9e64d6e
Oto Brglez <otobrglez@gmail.com>
2011-06-29 14:14:15 +0200

    Update...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ SvezeJabolko::Application.configure do
   config.action_controller.asset_host = "http://sveze-jabolko-common.s3.amazonaws.com"
   
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false # true if heroku
+  config.serve_static_assets = true #false # true if heroku
 
   
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
