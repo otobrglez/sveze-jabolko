cb264f84bba35800df765aca7746c50c774879f4
Oto Brglez <otobrglez@gmail.com>
2011-09-19 14:57:57 +0200

    Updating tags admin

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -6,7 +6,7 @@ class Admin::TagsController < AdminController
   respond_to :html, :json
   
   def index
-    @tags = Article.tag_counts_on(:tags)
+    @tags = Article.tag_counts_on(:tags, :order => "name ASC")
   end
   
   def search #?term=ab
@@ -21,6 +21,40 @@ class Admin::TagsController < AdminController
           { id: t.name, label: t.name, value: t.name }
         }.to_json
       }
+    end
+  end
+
+  def destroy
+    @tag ||= ActsAsTaggableOn::Tag.find(params[:id])
+    return redirect_to [:admin,:tags] if @tag.nil?
+    notice = "Tag odstranjen!" if @tag.destroy
+
+    respond_with(@tag) do |f|
+      f.html {  redirect_to [:admin,:tags] }
+      f.js{ return render :destroy, :layout => false }
+    end
+   
+  end
+
+  def edit
+    @tag ||= ActsAsTaggableOn::Tag.find(params[:id])
+    respond_with(@tag) do |f|
+      f.js { return render :edit, :layout => false}
+    end
+  end
+
+  def update
+    @tag ||= ActsAsTaggableOn::Tag.find(params[:id])
+    @tag.update_attributes(params[:acts_as_taggable_on_tag])
+
+    respond_with(@tag) do |f|
+      f.js do
+        if @tag.save
+          return render :view, :layout => false
+        else
+          return render :edit, :layout => false
+        end
+      end
     end
   end
   
