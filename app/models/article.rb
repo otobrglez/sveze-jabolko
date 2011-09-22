4c2322c77d582c35492ea86289f833030f9b2e2f
Oto Brglez <otobrglez@gmail.com>
2011-09-22 15:14:20 +0200

    Update

--- app/models/article.rb
+++ app/models/article.rb
@@ -42,7 +42,7 @@ class Article < ActiveRecord::Base
   end
   
   def publish_date
-    self.publish_date.strftime("%Y-%m-%d %H:%M:%S")
+    read_attribute(:publish_date).strftime("%Y-%m-%d %H:%M:%S")
   end
 
   def self.top_viewed(limit=10)
